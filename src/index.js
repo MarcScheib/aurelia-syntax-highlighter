3edfcf9c1a05a30b42200030f1fb8f534ad77e50
Marc Scheib <marc.scheib@gmx.de>
2016-05-22 19:57:15 +0200

    chore(*): fix exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-export function configure(config) {
+import {SyntaxHighlighter} from './syntax-highlighter';
+
+function configure(config) {
   config.globalResources(
     './syntax-highlighter'
   );
 }
+
+export {
+  SyntaxHighlighter,
+  configure
+};
